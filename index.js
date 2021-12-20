62a7200b97d8161bbfd41675d80d3d9e7eadcc70
chitranshjain <cjsmcs2810@gmail.com>
2021-12-20 13:41:18 +0530

    changes

--- index.js
+++ index.js
@@ -17,16 +17,16 @@ const io = require('socket.io').listen(server);
 dotenv.config();
 app.use(bodyParser.json());
 
-// // CORS Rules Modification
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-//   next();
-// });
+// CORS Rules Modification
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
 
 app.use(cors('dev'))
 
